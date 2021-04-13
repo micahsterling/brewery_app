8946aeba59d35cdfa554cb0dc597697f94b0d52b
Mat <mvello66@gmail.com>
2021-04-12 21:35:40 -0400

    populates table with CSV data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'csv'
 
-require 'csv'
+require "csv"
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv_text = File.read(Rails.root.join("lib", "seeds", "breweries_us.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
 csv.each do |row|
   t = Brewery.new
-  t.brewery_name = row['brewery_name']
-  t.type = row['type']
-  t.address = row['address']
-  t.website = row['website']
-  t.state = row['state']
-  t.state_breweries = row['state_breweries']
+  t.brewery_name = row["brewery_name"]
+  t.brewery_type = row["type"]
+  t.address = row["address"]
+  t.website = row["website"]
+  t.state = row["state"]
+  t.state_breweries = row["state_breweries"]
   t.save
 end
 
